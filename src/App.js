f18b8d7c62ec04f2c871ca587f4e51bae14de852
World Wide <agwalker249@gmail.com>
2019-09-19 12:16:54 -0500

    Mobile styling changes, added README, added NoMatch page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Rules from "./pages/Rules";
 import Nav from "./components/Nav";
-// import NoMatch from "./pages/NoMatch";
+import NoMatch from "./pages/NoMatch";
 import "./App.css";
 
 const App = () => 
@@ -15,7 +15,7 @@ const App = () =>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/rules" component={Rules} />
-        {/* <Route component={NoMatch} /> */}
+        <Route component={NoMatch} />
       </Switch>
     </div>
   </Router>
--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -1,22 +1,19 @@
-// import React from "react";
-// import { Col, Row, Container } from "../../components/Grid";
-// import Jumbotron from "../../components/Jumbotron";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "reactstrap";
 
-// const NoMatch = () => (
-//   <Container fluid>
-//     <Row>
-//       <Col size="md-12">
-//         <Jumbotron>
-//           <h1>404 Page Not Found</h1>
-//           <h1>
-//             <span role="img" aria-label="Face With Rolling Eyes Emoji">
-//               🙄
-//             </span>
-//           </h1>
-//         </Jumbotron>
-//       </Col>
-//     </Row>
-//   </Container>
-// );
+const NoMatch = () => (
+  <Container fluid>
+    <Link to={"/"}><button id="backBtn"><i class="fas fa-long-arrow-alt-left"></i> Back</button></Link>
+    <Row>
+      <Col size="md-12">
+          <h4>404 Page Not Found <span role="img" aria-label="Face With Rolling Eyes Emoji">
+              🙄
+            </span>
+          </h4>
+      </Col>
+    </Row>
+  </Container>
+);
 
-// export default NoMatch;
+export default NoMatch;
